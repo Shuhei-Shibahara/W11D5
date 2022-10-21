96395d795443337c7fee35b7e848bfac4d2117c1
Shuhei Shibahara <Shoehey06@gmail.com>
2022-10-21 14:55:09 -0700

    finished models

--- pokedex-rails-backend/app/models/move.rb
+++ pokedex-rails-backend/app/models/move.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: moves
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Move < ApplicationRecord
+  validates :name, presence: true, uniqueness: {message: "'%{value}' is already in use"}
+
+  has_many :pokemon,
+  dependent: :destroy
 end
--- pokedex-rails-backend/app/models/poke_move.rb
+++ pokedex-rails-backend/app/models/poke_move.rb
@@ -1,4 +1,18 @@
+# == Schema Information
+#
+# Table name: poke_moves
+#
+#  id         :bigint           not null, primary key
+#  move_id    :bigint           not null
+#  pokemon_id :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class PokeMove < ApplicationRecord
+  validates :pokemon_id, uniqueness: {scope: :move_id, 
+    message: "You already learned this move"
+  }
+
   belongs_to :move
   belongs_to :pokemon
 end
