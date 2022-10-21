0f678b12850305a19f030df69bfa70adebc76fb8
Shuhei Shibahara <Shoehey06@gmail.com>
2022-10-21 16:17:24 -0700

    finished routes

--- pokedex-rails-backend/config/routes.rb
+++ pokedex-rails-backend/config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api, defaults: {format: :json} do 
+
+    get "/pokemon/types", to: 'pokemon#types'
+
+    resources :pokemon, only: [:index, :create, :update]
+
+    resources :pokemon, only: [:show] do 
+      resources :items, only: [:index, :create]
+    end
+
+    resources :items, only: [:update, :delete]
+
+
+  end
 end
